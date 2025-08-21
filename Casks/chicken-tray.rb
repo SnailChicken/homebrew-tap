1b5da33cb2fe4724a5ed71e32d54b6003d65c08e
Eli Spain <elispain@Elis-Mac-mini.local>
2025-08-21 13:06:31 -0700

    Add Chicken Tray cask

---
+++ Casks/chicken-tray.rb
@@ -0,0 +1,9 @@
+cask "chicken-tray" do
+  version :latest
+  sha256 :no_check
+  url "https://github.com/SnailChicken/Chicken-Tray/releases/download/Blank/ChickenTray.dmg"
+  name "Chicken Tray"
+  desc "Tray app for chickens"
+  homepage "https://github.com/SnailChicken/Chicken-Tray"
+  app "Chicken Tray.app"
+end
