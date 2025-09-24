cf3cb3f591c8015a730c7778e29252acce4a21cb
SnailChicken <ezspain@icloud.com>
2025-09-24 16:35:18 -0700

    Add files via upload

---
+++ Casks/chicken-land.rb
@@ -0,0 +1,12 @@
+cask "chicken-land" do
+  version "1.0.0"
+  sha256 "7a80e7186545e72ecef0c9b37ed3dddee1d14ed6a404a6558b190c44f4b36211"
+
+  url "https://github.com/SnailChicken/Chicken-Land/releases/download/blank/Chicken.Land.1.0.0.dmg",
+      verified: "github.com/SnailChicken/Chicken-Land/"
+  name "Chicken Land"
+  desc "Electron wrapper for ez-spain.com/chickens"
+  homepage "https://ez-spain.com/chickens"
+
+  app "Chicken Land.app"
+end
