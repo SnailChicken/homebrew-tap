6fb5148dedc3b709fd36f06cdc34807bd12d896f
SnailChicken <ezspain@icloud.com>
2025-09-24 17:36:31 -0700

    Update chicken-tray.rb

--- Casks/chicken-tray.rb
+++ Casks/chicken-tray.rb
@@ -1,7 +1,7 @@
 cask "chicken-tray" do
   version :latest
   sha256 :no_check
-  url "https://github.com/SnailChicken/Chicken-Tray/releases/download/V2/Chicken.Tray.app.zip"
+  url "https://github.com/SnailChicken/Chicken-Tray/releases/download/V2/Chicken.Tray.dmg"
   name "Chicken Tray"
   desc "Tray app for chickens"
   homepage "https://ez-spain.com/chickentrayapp"
