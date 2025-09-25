317bd6b4aa6e61822e97636efea99feb181bd221
SnailChicken <ezspain@icloud.com>
2025-09-24 17:35:27 -0700

    Update chicken-land.rb

--- Casks/chicken-land.rb
+++ Casks/chicken-land.rb
@@ -2,7 +2,7 @@ cask "chicken-land" do
   version "1.0.0"
   sha256 "7a80e7186545e72ecef0c9b37ed3dddee1d14ed6a404a6558b190c44f4b36211"
 
-  url "https://github.com/SnailChicken/Chicken-Land/releases/download/V2/Chicken.Land.app.zip",
+  url "https://github.com/SnailChicken/Chicken-Land/releases/download/V2/Chicken.Land.dmg",
       verified: "github.com/SnailChicken/Chicken-Land/"
   name "Chicken Land"
   desc "Cute game with chickens"
