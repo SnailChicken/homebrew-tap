de1a09c4e507d0efd2a62d754c547e4dceee6ac9
SnailChicken <ezspain@icloud.com>
2025-10-15 12:26:29 -0700

    Fix sha256 hash format in menushell.rb

--- Casks/menushell.rb
+++ Casks/menushell.rb
@@ -1,6 +1,6 @@
 cask "menushell" do
   version "1.0.0"
-  sha256 "sha256:106cc140bc311f7c9742d6c968f713b558cb681e5eeb55f1ef510acf19672ef1"
+  sha256 "106cc140bc311f7c9742d6c968f713b558cb681e5eeb55f1ef510acf19672ef1"
 
   url "https://github.com/SnailChicken/MenuShell/releases/download/V1/MenuShell.dmg",
       verified: "github.com/SnailChicken/MenuShell"
