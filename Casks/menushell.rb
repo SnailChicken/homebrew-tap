f2406fd223488a59e94ec3e72e1a8ae4a295d8b1
SnailChicken <ezspain@icloud.com>
2025-10-15 12:23:45 -0700

    Fix SHA256 checksum for MenuShell cask
    Updated the SHA256 checksum for MenuShell cask.

--- Casks/menushell.rb
+++ Casks/menushell.rb
@@ -1,6 +1,6 @@
 cask "menushell" do
   version "1.0.0"
-  sha256 "8c619a46cf2b03a3f7a208d671cd071af07dff37a04eff181d2f99aa03a8a91e"
+  sha256 "sha256:106cc140bc311f7c9742d6c968f713b558cb681e5eeb55f1ef510acf19672ef1"
 
   url "https://github.com/SnailChicken/MenuShell/releases/download/V1/MenuShell.dmg",
       verified: "github.com/SnailChicken/MenuShell"
