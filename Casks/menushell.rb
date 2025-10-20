20c386d6118e47cd17c185f7a2232427461556c2
SnailChicken <ezspain@icloud.com>
2025-10-20 13:17:47 -0700

    Update MenuShell to version 2.0.0

--- Casks/menushell.rb
+++ Casks/menushell.rb
@@ -1,8 +1,8 @@
 cask "menushell" do
-  version "1.0.0"
-  sha256 "106cc140bc311f7c9742d6c968f713b558cb681e5eeb55f1ef510acf19672ef1"
+  version "2.0.0"
+  sha256 "1935fc9d43f29ed6fd0899d8a17eaea09b5d352de90f139d6167bbd84f88c625"
 
-  url "https://github.com/SnailChicken/MenuShell/releases/download/V1/MenuShell.dmg",
+  url "https://github.com/SnailChicken/MenuShell/releases/download/V2/MenuShell.dmg",
       verified: "github.com/SnailChicken/MenuShell"
   name "MenuShell"
   desc "A terminal for the menu bar"
